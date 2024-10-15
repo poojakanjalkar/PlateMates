45a8f68d6ce32fdca482bd973e1dc28677f73976
Pooja Kanjalkar <poojakanjalkar554@gmail.com>
2024-10-15 10:04:55 +0530

    used usePathname to highlight the route on which user is

--- 01-starting-project/components/main-header.js
+++ 01-starting-project/components/main-header.js
@@ -3,8 +3,10 @@ import logoImg from "@/assets/logo.png";
 import classes from "./main-header.module.css";
 import Image from "next/image";
 import MainHeaderBackground from "./main-header-background";
+import { usePathname } from "next/navigation";
 
 export default function MainHeader() {
+  const path = usePathname();
   return (
     <>
       <MainHeaderBackground />
@@ -17,10 +19,24 @@ export default function MainHeader() {
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link href="/meals">Browse meals</Link>
+              <Link
+                href="/meals"
+                className={
+                  path.startsWith("/meals") ? classes.active : undefined
+                }
+              >
+                Browse meals
+              </Link>
             </li>
             <li>
-              <Link href="/community">Foodies Community</Link>
+              <Link
+                href="/community"
+                className={
+                  path.startsWith("/community") ? classes.active : undefined
+                }
+              >
+                Foodies Community
+              </Link>
             </li>
           </ul>
         </nav>
